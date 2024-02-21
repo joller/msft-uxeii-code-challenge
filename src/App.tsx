109edf884ece6edccb48b44e7f50c47eac813960
Jason Oller <jayoller@gmail.com>
2024-02-20 20:11:10 -0500

    getting results by breed

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,90 @@ import Search from './Search'
 
 import './App.css';
 import type { RandomDog } from './services/getDogs';
-import { getRandomDogs } from './services/getDogs';
+import { getAllDogs, getRandomDogs, getDogsByBreed } from './services/getDogs';
 import { parseUrlByBreed } from './utilities/parseUrlByBreed';
 
 function App() {
   const [results, setResults] = useState([])
+  const [searchData, setSearchData] = useState()
+  const [matchedResultsArr, setmatchedResultsArr] = useState([])
+
+  //TODO 
+  // create search data that gives us all dog possibilities
+  // filter/find using searchTerm to map through that search data, and return only the matches
+  // setResults to the matched search data
+
+  // TODO have this populate the URL when searchQuery is populated? Could be the "nice to have" 
+  // Will need to encode the url param
+  // const [searchQuery, setSearchQuery] = useState('')
+
+  function findSearchMatch(searchKey: string, obj: any): any | undefined {
+    const matchKey = Object.keys(obj).filter(key => key.includes(searchKey));
+    if (matchKey) {
+        return matchKey;
+    } else {
+        return undefined;
+    }
+}
 
   useEffect(() => {
-    // getRandomDogs(12).then(data => {
-    //   setResults(data.message)
-    //  });
-
     getRandomDogs(12).then(data => {
       const imageData = data.message
 
-      const enhancedResults = imageData.map((imageData: string) => {
+      // sets the initial results display
+      const initialDisplayResults = imageData.map((imageData: string) => {
         const labelResult = parseUrlByBreed(imageData, 'breeds/')
         return {img: imageData, label: labelResult }
       })
-      setResults(enhancedResults)
+      setResults(initialDisplayResults)
     })
 
-    console.log(results)
+    getAllDogs().then(data => {
+      const dogData = data.message;
+
+      setSearchData(dogData)
+    })
   }, [])
 
   /// TODO search code
+  const submitSearch = (e: any) => {
+    // Prevents browser from reloading the page
+    e.preventDefault();
+
+    // Read the form data
+    const form = e.target;
+
+    // Constructor to create key/val pairs from submitted value
+    const formData = new FormData(form);
+
+    // Convert to a plain object
+    const formJson = Object.fromEntries(formData.entries())
+
+    const searchTerm = formJson.search.toString();
+
+    const searchMatchResult = findSearchMatch(searchTerm, searchData)
+
+    if (matchedResultsArr.length < searchMatchResult.length) {
+
+      searchMatchResult.forEach(breed => {      
+        getDogsByBreed(breed).then(data => {
+          // Getting image data
+          const imageData = data.message
+          // creating an object that has imgData and label
+          const enhancedBreedObj = {img: imageData, label: breed.toString()}
+          matchedResultsArr.push(enhancedBreedObj)
+          
+          
+        })
+      });
+    } else {
+      setmatchedResultsArr(matchedResultsArr)
+      setResults(matchedResultsArr)
+      console.log(matchedResultsArr)
+
+    }
+
+  }
 
 
   return (
@@ -43,14 +102,15 @@ function App() {
         An Innovation & Technology team challenge
       </p> */ }
        <div className="flex items-center justify-center search-wrapper">
+       <form action="" method="get" name="search" onSubmit={submitSearch}>
             <label className="sr-only" htmlFor="search">Search</label>
-            <input id="searchInput" className="search-input" placeholder="Search" type="text" />
+            <input id="searchInput" className="search-input" name="search" placeholder="Search" type="text" />
             <button className="button">Search</button>
+        </form>
         </div>
     </header>
     <div className="results">
       <ul className="results-list">
-
         {
         results.map((result: RandomDog, i) => (
         <li className="result-item" key={i}> 
--- src/services/getDogs.ts
+++ src/services/getDogs.ts
@@ -54,4 +54,18 @@ export const getRandomDogs = async (amount: number): Promise<RandomDogsResponse>
 };
 
 
+export const getDogsByBreed = async (breed: string) => {
+  const url = `https://dog.ceo/api/breed/${breed}/images/random`;
 
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Failed to fetch data:', error);
+  }
+};
