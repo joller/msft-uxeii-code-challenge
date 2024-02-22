1feaf549405c8185e62f98aa7dc440d7b84f4eb1
Jason Oller <jayoller@gmail.com>
2024-02-22 00:18:30 -0500

    modal updates

--- src/DogModal.tsx
+++ src/DogModal.tsx
@@ -9,9 +9,11 @@ export const DogModal = (props: ModalProps) => {
   return (
     <div className="modal flex justify-center items-center">
       <div className="modal-inner">
-        <div className="flex justify-between">
+        <div className="flex justify-between items-center modal-header">
           <h2 className="modal-title">{info.breed}</h2>
-          <button onClick={() => handleClose()}>Close</button>
+          <button className="button" onClick={() => handleClose()}>
+            Close
+          </button>
         </div>
 
         <img className="modal-image" src={info.img} alt={info.breed} />
